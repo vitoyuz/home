8c95f35cf46087f1a54294d8d30501cd17aa5787
vitoyuz <vitoyuz@sina.com>
2015-11-15 11:58:41 +0800

    complete

--- bin/init.sh
+++ bin/init.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+sudo=""
+
+if [ `uname -a` | grep "Darwin" = "" ]
+then
+  echo "os is linux"
+  sudo="sudo"
+  $sudo apt-get install vim
+  $sudo apt-get install zsh
+else
+  echo "os is osx"
+fi
+
 #zsh init
+echo "zsh init"
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s /bin/zsh
+$sudo chsh -s /bin/zsh
 
 #vim init
+echo "vim init"
 git clone https://github.com/gmarik/vundle.git  ~/.vim/bundle/vundle
 vim -c BundleInstall &
 
